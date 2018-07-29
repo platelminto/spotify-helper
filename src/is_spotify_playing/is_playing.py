98fe9993ec24473b60717e514913ef3eaf59f0ed
platelminto <gmomigliano@gmail.com>
2018-07-30 00:25:14 +0200

    Made is_playing imports local to each OS

--- src/is_spotify_playing/is_playing.py
+++ src/is_spotify_playing/is_playing.py
@@ -6,8 +6,6 @@ import os
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
-import linux_playing, mac_playing, windows_playing
-
 
 def fire_when_playing(event):
 
@@ -21,10 +19,13 @@ def start_listening(interval, daemon=True):
     current_os = platform.system()
 
     if current_os == 'Linux':
+        import linux_playing
         target = linux_playing.is_now_playing
     if current_os == 'Darwin':
+        import mac_playing
         target = mac_playing.is_now_playing
     if current_os == 'Windows':
+        import windows_playing
         target = windows_playing.is_now_playing
 
     t = threading.Thread(target=target, args=(interval,))
