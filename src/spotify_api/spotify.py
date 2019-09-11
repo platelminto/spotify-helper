1976cde6d5c9c1109c0771342fcedff7204f009a
Giorgio Momigliano <gmomigliano@protonmail.com>
2019-09-11 02:24:40 +0100

    Rearranged methods for clearer order, starting with ones called directly form bindings

--- src/spotify_api/spotify.py
+++ src/spotify_api/spotify.py
@@ -49,141 +49,6 @@ class Spotify:
 
         self.repeat_states = ['track', 'context', 'off']
 
-    def __fetch_user_id(self):
-        return self.web_api.get('me').json().get('id')
-
-    def get_monthly_playlist_id(self):
-        now = datetime.datetime.now()
-        month, year = now.strftime('%B'), str(now.year)
-
-        with shelve.open('../.info') as shelf:
-            # Check if months and years are available and are correct, if not, update
-            # playlist id.
-            if 'month' not in shelf or shelf['month'] != month:
-                shelf['month'] = month
-                shelf['monthly_playlist_id'] = self.__fetch_playlist_id(month, year)
-            if 'year' not in shelf or shelf['year'] != year:
-                shelf['year'] = year
-                shelf['monthly_playlist_id'] = self.__fetch_playlist_id(month, year)
-            return shelf['monthly_playlist_id']
-
-    def get_user_id(self):
-        with shelve.open('../.info') as shelf:
-            if 'user_id' not in shelf:
-                shelf['user_id'] = self.__fetch_user_id()
-            return shelf['user_id']
-
-    def __fetch_playlist_id(self, month, year):
-        response = self.web_api.get('me/playlists').json()
-
-        for playlist in response.get('items'):
-            if str(playlist.get('name')).lower() == (month + ' ' + year).lower():
-                return playlist.get('id')
-
-        return self.web_api.post('users/' + self.get_user_id() + '/playlists',
-                                 payload={'name': (month.capitalize() + ' ' + year)}).json().get('id')
-
-    def add_song_to_monthly_playlist(self, song_id):
-        return self.web_api.post(
-            'users/' + self.get_user_id() + '/playlists/' + self.get_monthly_playlist_id() + '/tracks',
-            params={'uris': 'spotify:track:' + song_id})
-
-    def remove_song_from_monthly_playlist(self, song_id):
-        return self.web_api.delete(
-            'users/' + self.get_user_id() + '/playlists/' + self.get_monthly_playlist_id() + '/tracks',
-            payload={'tracks': [{'uri': 'spotify:track:' + song_id}]})
-
-    def toggle_save_monthly_playlist(self):
-        song_id = self.get_current_song_id()
-        is_in_playlist = self.is_in_playlist(song_id)
-
-        song = self.get_current_song_info()[0]
-
-        if is_in_playlist:
-            self.remove_song_from_monthly_playlist(song_id)
-            send_notif_with_web_image('Successfully removed',
-                                      'Removed ' + song + ' from playlist.',
-                                      self.currently_playing_art_url())
-        else:
-            self.add_song_to_monthly_playlist(song_id)
-            send_notif_with_web_image('Successfully added',
-                                      'Added ' + song + ' to playlist.',
-                                      self.currently_playing_art_url())
-
-    def is_in_playlist(self, song_id):
-        playlist_tracks = self.web_api.get('users/' + self.get_user_id() +
-                                           '/playlists/' + self.get_monthly_playlist_id() +
-                                           '/tracks').json().get('items')
-
-        return len([x for x in playlist_tracks if x.get('track').get('id') == song_id]) > 0
-
-    def add_songs_to_library(self, *song_ids):
-        return self.web_api.put('me/tracks', payload={'ids': song_ids})
-
-    def get_available_devices(self):
-        return self.web_api.get('me/player/devices').json().get('devices')
-
-    # The active device is the one currently playing music, not necessarily the one currently
-    # being used by the user (for example, music could be playing through a phone, but the user
-    # is on their computer).
-    def get_active_device(self):
-        devices = self.get_available_devices()
-        try:
-            return next(device for device in devices if device.get('is_active'))
-
-        except StopIteration:
-            return None
-
-    def show_current_song(self):
-        song, artists, album = self.get_current_song_info()
-        send_notif_with_web_image(song, ', '.join(artists) + ' - ' + album, self.currently_playing_art_url())
-
-    def get_current_song_info(self):
-        def get_track_from_web_api(response):
-            return response.json().get('item')
-
-        track = self.try_local_method_then_web('get_current_track', '', 'get', get_track_from_web_api)
-
-        song = track.get('name')
-        artists = [x.get('name') for x in track.get('artists')]
-        album = track.get('album').get('name')
-
-        return song, artists, album
-
-    def get_current_song_id(self):
-        def get_id_from_web_api(response):
-            return response.json().get('item').get('id')
-
-        return self.try_local_method_then_web('get_track_id', '', 'get', get_id_from_web_api, 'get')
-
-    def is_saved(self, song_id):
-        return self.web_api.get('me/tracks/contains?ids=' + song_id).json()[0]
-
-    def currently_playing_art_url(self, track=None, quality=2):
-        if track is None:
-            try:
-                track = self.try_local_method_then_web('', '', 'get').json().get('item')
-
-            except ConnectionError:
-                return None
-
-        images = track.get('album').get('images')
-
-        # We don't need very high quality images for notifications, so we get
-        # the images at the end of the list (which is ordered by quality).
-        return images[-quality if len(images) >= 2 else 0].get('url')
-
-    def remove_songs_from_library(self, *song_ids):
-        return self.web_api.delete('me/tracks', payload={'ids': song_ids})
-
-    def is_playing(self):
-        return self.try_local_method_then_web('is_playing', '', 'get').json().get('is_playing')
-
-    # Utility method for possible future use.
-    def get_shuffle_and_repeat_state(self):
-        response = self.web_api.get('me/player').json()
-        return response.get('shuffle_state'), response.get('repeat_state')
-
     def next(self):
         self.try_local_method_then_web('next', 'next', 'post')
 
@@ -262,6 +127,142 @@ class Spotify:
 
     def play_on_current_device(self):
         self.call_player_method('', 'put', payload={'device_ids': [self.get_current_device_id()]})
+
+    def toggle_save_monthly_playlist(self):
+        song_id = self.get_current_song_id()
+        is_in_playlist = self.is_in_playlist(song_id)
+
+        song = self.get_current_song_info()[0]
+
+        if is_in_playlist:
+            self.remove_song_from_monthly_playlist(song_id)
+            send_notif_with_web_image('Successfully removed',
+                                      'Removed ' + song + ' from playlist.',
+                                      self.currently_playing_art_url())
+        else:
+            self.add_song_to_monthly_playlist(song_id)
+            send_notif_with_web_image('Successfully added',
+                                      'Added ' + song + ' to playlist.',
+                                      self.currently_playing_art_url())
+
+    def show_current_song(self):
+        song, artists, album = self.get_current_song_info()
+        send_notif_with_web_image(song, ', '.join(artists) + ' - ' + album, self.currently_playing_art_url())
+
+    def add_song_to_monthly_playlist(self, song_id):
+        return self.web_api.post(
+            'users/' + self.get_user_id() + '/playlists/' + self.get_monthly_playlist_id() + '/tracks',
+            params={'uris': 'spotify:track:' + song_id})
+
+    # The API is inconsitent so adding and deleting are different.
+    def remove_song_from_monthly_playlist(self, song_id):
+        return self.web_api.delete(
+            'users/' + self.get_user_id() + '/playlists/' + self.get_monthly_playlist_id() + '/tracks',
+            payload={'tracks': [{'uri': 'spotify:track:' + song_id}]})
+
+    def get_current_song_info(self):
+        def get_track_from_web_api(response):
+            return response.json().get('item')
+
+        track = self.try_local_method_then_web('get_current_track', '', 'get', get_track_from_web_api)
+
+        song = track.get('name')
+        artists = [x.get('name') for x in track.get('artists')]
+        album = track.get('album').get('name')
+
+        return song, artists, album
+
+    def get_monthly_playlist_id(self):
+        now = datetime.datetime.now()
+        month, year = now.strftime('%B'), str(now.year)
+
+        with shelve.open('../.info') as shelf:
+            # Check if months and years are available and are correct, if not, update
+            # playlist id.
+            if 'month' not in shelf or shelf['month'] != month:
+                shelf['month'] = month
+                shelf['monthly_playlist_id'] = self.__fetch_playlist_id(month, year)
+            if 'year' not in shelf or shelf['year'] != year:
+                shelf['year'] = year
+                shelf['monthly_playlist_id'] = self.__fetch_playlist_id(month, year)
+            return shelf['monthly_playlist_id']
+
+    def get_user_id(self):
+        with shelve.open('../.info') as shelf:
+            if 'user_id' not in shelf:
+                shelf['user_id'] = self.__fetch_user_id()
+            return shelf['user_id']
+
+    def __fetch_user_id(self):
+        return self.web_api.get('me').json().get('id')
+
+    def __fetch_playlist_id(self, month, year):
+        response = self.web_api.get('me/playlists').json()
+
+        for playlist in response.get('items'):
+            if str(playlist.get('name')).lower() == (month + ' ' + year).lower():
+                return playlist.get('id')
+
+        return self.web_api.post('users/' + self.get_user_id() + '/playlists',
+                                 payload={'name': (month.capitalize() + ' ' + year)}).json().get('id')
+
+    def is_in_playlist(self, song_id):
+        playlist_tracks = self.web_api.get('users/' + self.get_user_id() +
+                                           '/playlists/' + self.get_monthly_playlist_id() +
+                                           '/tracks').json().get('items')
+
+        return len([x for x in playlist_tracks if x.get('track').get('id') == song_id]) > 0
+
+    def add_songs_to_library(self, *song_ids):
+        return self.web_api.put('me/tracks', payload={'ids': song_ids})
+
+    def get_available_devices(self):
+        return self.web_api.get('me/player/devices').json().get('devices')
+
+    # The active device is the one currently playing music, not necessarily the one currently
+    # being used by the user (for example, music could be playing through a phone, but the user
+    # is on their computer).
+    def get_active_device(self):
+        devices = self.get_available_devices()
+        try:
+            return next(device for device in devices if device.get('is_active'))
+
+        except StopIteration:
+            return None
+
+    def get_current_song_id(self):
+        def get_id_from_web_api(response):
+            return response.json().get('item').get('id')
+
+        return self.try_local_method_then_web('get_track_id', '', 'get', get_id_from_web_api, 'get')
+
+    def is_saved(self, song_id):
+        return self.web_api.get('me/tracks/contains?ids=' + song_id).json()[0]
+
+    def currently_playing_art_url(self, track=None, quality=2):
+        if track is None:
+            try:
+                track = self.try_local_method_then_web('', '', 'get').json().get('item')
+
+            except ConnectionError:
+                return None
+
+        images = track.get('album').get('images')
+
+        # We don't need very high quality images for notifications, so we get
+        # the images at the end of the list (which is ordered by quality).
+        return images[-quality if len(images) >= 2 else 0].get('url')
+
+    def remove_songs_from_library(self, *song_ids):
+        return self.web_api.delete('me/tracks', payload={'ids': song_ids})
+
+    def is_playing(self):
+        return self.try_local_method_then_web('is_playing', '', 'get').json().get('is_playing')
+
+    # Utility method for possible future use.
+    def get_shuffle_and_repeat_state(self):
+        response = self.web_api.get('me/player').json()
+        return response.get('shuffle_state'), response.get('repeat_state')
 
     def get_current_device_id(self):
         return next(x.get('id') for x in self.call_player_method('devices', 'get').json().get('devices') if
